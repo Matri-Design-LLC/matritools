5dd053e6e8f36925f60691d89312817a29c91c87
BradleyGBrandt <59422589+BradleyGBrandt@users.noreply.github.com>
2021-09-08 13:45:25 -0700

    changed encoding of node and tag files to utf-8

--- matritools/nodefile.py
+++ matritools/nodefile.py
@@ -97,8 +97,13 @@ class NodeFile:
             raise RuntimeError("Node File contains duplicate IDs.\n\nID | Indexes:\n\n" +
                                result + str(temp_nf.to_dataframe().to_string()))
 
+<<<<<<< Updated upstream
         node_file = open(self.node_file_name + "_node.csv", "w")
         tag_file = open(self.node_file_name + "_tag.csv", "w")
+=======
+        node_file = open(self.__node_file_name__ + "_node.csv", "w", encoding="utf-8")
+        tag_file = open(self.__node_file_name__ + "_tag.csv", "w", encoding="utf-8")
+>>>>>>> Stashed changes
         tag_file.write("id,record_id,table_id,title,description\n")
 
         node_file.write(self.header)
