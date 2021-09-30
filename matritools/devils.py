85c1fcc09c280f4a8ae32a49a974c96a82a0b0d9
BradleyGBrandt <59422589+BradleyGBrandt@users.noreply.github.com>
2021-09-29 17:03:19 -0700

    Added devils module to matritools

---
+++ matritools/devils.py
@@ -0,0 +1,13 @@
+position_colors = {
+    'Goalie': 'white',
+    'Center/R.Wing': 'pink',
+    'Left Wing': 'blue',
+    'Forward': 'yellow',
+    'Right Wing': 'red',
+    'Defence': 'cyan',
+    'Center': 'magenta',
+    'Right Defence': 'orange',
+    'Center/L.Wing': 'grey',
+    'Wing': 'green',
+    'Left Defence': 'lime'
+}
