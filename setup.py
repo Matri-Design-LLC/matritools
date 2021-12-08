7fe88808a5aeea4a8371d8363d2ce95bb6277b1b
BradleyGBrandt <59422589+BradleyGBrandt@users.noreply.github.com>
2021-12-08 13:26:27 -0800

    added .readthedocs.yaml

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='matritools',
-    version='0.0.4',
+    version='0.1.0',
     packages=find_packages(include=['matritools', 'matritools.*']),
     install_requires=[
         'pandas>=1.1.3',
